cc812328e17054d93e4bf3824fefe0b472086775
Ami-OS <www.69929536@gmail.com>
2021-05-09 15:17:13 +0800

    Init

---
+++ js.js
@@ -0,0 +1,109 @@
+document.addEventListener('DOMContentLoaded', () => {
+    /*
+     * autosize init
+     */
+    autosize(document.querySelectorAll('textarea'));
+
+    /*
+     * content
+     */
+    var tmp_ar = [];
+    // document.querySelectorAll('.content .input-group-text').forEach(e => tmp_ar.push(e.offsetWidth));
+    document.querySelectorAll('.content .input-group-text').forEach(e => tmp_ar.push(e.getBoundingClientRect().width));
+    var $_content_input_group_text = Math.max.apply(null, tmp_ar);
+    document.querySelectorAll('.content .input-group-text').forEach(e => e.style.width = $_content_input_group_text + 'px');
+
+    /*
+     * download button
+     */
+    $('#download').on('click', function() {
+        var pdf, page_section, HTML_Width, HTML_Height, top_left_margin, PDF_Width, PDF_Height, canvas_image_width, canvas_image_height;
+        var img_format = 'JPEG';
+
+        function calculatePDF_height_width(selector, index) {
+            page_section = $(selector).eq(index);
+            HTML_Width = page_section.width();
+            HTML_Height = page_section.height();
+            top_left_margin = 15;
+
+            PDF_Width = HTML_Width + (top_left_margin * 2);
+            PDF_Height = HTML_Height + (top_left_margin * 2);
+            
+            canvas_image_width = HTML_Width;
+            canvas_image_height = HTML_Height;
+        }
+
+        // Generate PDF
+        function generatePDF() {
+            // pdf = "";
+
+            // // Button hide
+            // $("#download").prop('disabled', true).text('Generating PDF ...');
+
+            // html2canvas($("main.page").eq(0)[0], { allowTaint: true }).then(function (canvas) {
+
+            //     document.body.appendChild(canvas);
+
+            //     calculatePDF_height_width("main.page", 0);
+            //     var imgData = canvas.toDataURL("image/png", 1.0);
+
+            //     // console.log(imgData);
+
+            //     pdf = new jspdf.jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+            //     pdf.addImage(imgData, img_format, top_left_margin, top_left_margin, HTML_Width, HTML_Height);
+
+            // });
+
+            // // html2canvas($(".print-wrap:eq(1)")[0], { allowTaint: true }).then(function (canvas) {
+
+            // //     calculatePDF_height_width(".print-wrap", 1);
+            // //     var imgData = canvas.toDataURL("image/png", 1.0);
+            // //     pdf.addPage([PDF_Width, PDF_Height]);
+            // //     pdf.addImage(imgData, img_format, top_left_margin, top_left_margin, HTML_Width, HTML_Height);
+
+            // // });
+
+            // html2canvas($("main.page").eq(1)[0], { allowTaint: true }).then(function (canvas) {
+
+            //     calculatePDF_height_width("main.page", 1);
+            //     var imgData = canvas.toDataURL("image/png", 1.0);
+            //     pdf.addPage([PDF_Width, PDF_Height]);
+            //     pdf.addImage(imgData, img_format, top_left_margin, top_left_margin, HTML_Width, HTML_Height);
+
+            //     // console.log((page_section.length-1)+"==="+index);
+            //     setTimeout(function () {
+
+            //         // Get time
+            //         var dateTime = Date.now();
+            //         var timestamp = Math.floor(dateTime / 1000);
+
+            //         // Save PDF Doc
+            //         var PDF_NAME = "HTML-Document-" + timestamp + ".pdf";
+            //         pdf.save(PDF_NAME);
+
+            //         // Generate BLOB object
+            //         var blob = pdf.output("blob");
+
+            //         // Getting URL of blob object
+            //         var blobURL = URL.createObjectURL(blob);
+
+            //         // Button show
+            //         $('#download').prop('disabled', false).text('Download with PDF');
+
+            //     }, 0);
+            // });
+
+            html2canvas(document.querySelector("#page-1")).then(canvas => {
+                document.body.appendChild(canvas);
+            });
+        };
+
+        // Start generator
+        generatePDF();
+    });
+
+    /*
+     * loaded
+     */
+    document.querySelectorAll('.load-shell').forEach(e => e.classList.remove('load-shell'));
+});
